cf9035aa91a427aad7d0c6510f8fb822330f491e
Emilia Gromulska <emgromulska@gmail.com>
2020-03-25 11:25:43 +0100

    animals

--- src/Main.java
+++ src/Main.java
@@ -8,23 +8,27 @@ public class Main {
     public static void main(String[] args) {
 
         Kot kot1 = new Kot();
+        Kot kot2 = new Kot();
+        Kot kot3 = new Kot();
 
         List<Kot> kotList = new ArrayList<>();
-        kotList.add(new Kot());
-        kotList.add(new Kot());
-        kotList.add(new Kot());
+        kotList.add(kot1);
+        kotList.add(kot2);
+        kotList.add(kot3);
 
         System.out.println((kot1.dajGłos(6)));
 
         System.out.println(kotList);
 
         Pies pies1 = new Pies();
-        List <Pies> piesList = new ArrayList<>();
-        piesList.add(new Pies());
-        piesList.add(new Pies());
-        piesList.add(new Pies());
+        Pies pies2 = new Pies();
+        Pies pies3 = new Pies();
 
-        System.out.println((pies1.dajGłos(6)));
+        List <Pies> piesList = new ArrayList<>();
+        piesList.add(pies1);
+        piesList.add(pies2);
+        piesList.add(pies3);
+
 
         System.out.println(kotList);
         System.out.println(piesList);
